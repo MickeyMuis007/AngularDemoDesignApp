709e3681623f59f41147ba25ced90eaa92c8724c
Michael <mihendricks1@gmail.com>
2019-05-31 17:05:13 +0200

    Added Global configuration of accordions

--- src/app/app-bootstrap/app-bootstrap.module.ts
+++ src/app/app-bootstrap/app-bootstrap.module.ts
@@ -11,9 +11,10 @@ import { AccordionOpenPanelAtATimeComponent } from './components/bootstrap-accor
 import { AccordionToggelComponent } from './components/bootstrap-accordion/accordion-toggel/accordion-toggel.component';
 import { AccordionHeaderComponent } from './components/bootstrap-accordion/accordion-header/accordion-header.component';
 import { AccordionPreventChangeComponent } from './components/bootstrap-accordion/accordion-prevent-change/accordion-prevent-change.component';
+import { AccordionConfigComponent } from './components/bootstrap-accordion/accordion-config/accordion-config.component';
 
 @NgModule({
-  declarations: [BootstrapAccordionComponent, BootstrapAlertComponent, BootstrapAccordionDemoComponent, AccordionOpenPanelAtATimeComponent, AccordionToggelComponent, AccordionHeaderComponent, AccordionPreventChangeComponent],
+  declarations: [BootstrapAccordionComponent, BootstrapAlertComponent, BootstrapAccordionDemoComponent, AccordionOpenPanelAtATimeComponent, AccordionToggelComponent, AccordionHeaderComponent, AccordionPreventChangeComponent, AccordionConfigComponent],
   imports: [
     CommonModule,
     AppBootstrapRoutingModule,
